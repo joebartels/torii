07defedfcbe9b695e8bf27f83f2da05989259218
joseph bartels <joebartels@hotmail.com>
2014-08-18 18:22:38 -0400

    use window.__torii to determine if atorii popup is open

--- test/tests/unit/redirect-handler-test.js
+++ test/tests/unit/redirect-handler-test.js
@@ -16,6 +16,7 @@ test('exists', function(){
 });
 
 test('handles a url', function(){
+  window.opener.__torii = true;
   var url = "http://authServer?code=123451235fw";
   var handler = new RedirectHandler(url);
 
@@ -51,7 +52,8 @@ test('posts a message', function(){
   window.opener = {
     postMessage: function(message, origin){
       equal(message, "__torii_message:"+url, "posts back the url");
-    }
+    },
+    __torii: true
   };
 
   Ember.run(function(){
